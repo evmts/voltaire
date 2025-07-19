dca971cf2f57fae2bbfa4d3b4e80d728af299169
Will Cory <willcory10@gmail.com>
2025-07-18 23:31:38 -0700

    Update src/evm/memory/slice.zig

--- src/evm/memory/slice.zig
+++ src/evm/memory/slice.zig
@@ -2,7 +2,7 @@ const Memory = @import("./memory.zig").Memory;
 const context = @import("context.zig");
 
 /// Get a mutable slice to the entire memory buffer (context-relative)
-pub inline fn slice(self: *const Memory) []u8 {
+pub fn slice(self: *const Memory) []u8 {
     const ctx_size = self.context_size();
     const abs_start = self.my_checkpoint;
     const abs_end = @min(abs_start + ctx_size, self.shared_buffer_ref.items.len);
