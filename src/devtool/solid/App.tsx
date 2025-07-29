4d86dde9cc979bf5df2c70e2584e2fe40fc52b83
0xpolarzero <0xpolarzero@gmail.com>
2025-07-30 01:54:23 +0200

    refactor: add shadcn

--- src/devtool/solid/App.tsx
+++ src/devtool/solid/App.tsx
@@ -1,7 +1,13 @@
 import EvmDebugger from '~/components/evm-debugger/EvmDebugger'
+import { Toaster } from '~/components/ui/sonner'
 
 function App() {
-	return <EvmDebugger />
+	return (
+		<>
+			<EvmDebugger />
+			<Toaster />
+		</>
+	)
 }
 
 export default App
