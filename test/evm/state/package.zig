695fdeaddc6d06b4c4c3d1e7e8766ddaa4e6ed05
William Cory <williamcory@Williams-Mac-mini.local>
2025-08-24 01:25:45 -0700

    chore: Remove legacy tests

--- test/evm/state/package.zig
+++
@@ -1,9 +0,0 @@
-// State tests module
-
-pub const database_interface_test = @import("database_interface_test.zig");
-pub const journal_test = @import("journal_test.zig");
-
-test {
-    _ = database_interface_test;
-    _ = journal_test;
-}
