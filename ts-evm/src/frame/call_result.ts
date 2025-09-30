c6202513f7dc9054950641aa3923e4cce150e212
William Cory <williamcory@Williams-Mac-mini.local>
2025-09-30 16:01:17 -0700

    :fire: chore: rm ts-evm

--- ts-evm/src/frame/call_result.ts
+++
@@ -1,3 +0,0 @@
-export interface ReturnData { 
-  data: Uint8Array;
-}
--- ts-evm/test/sanity.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'bun:test';
-
-describe('sanity', () => {
-  it('runs a simple test', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
-
