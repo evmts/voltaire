b33d139fb498162a923eab4ac061c9d84bbf838e
William Cory <willcory10@gmail.com>
2025-10-28 16:10:30 -0700

    fix: broken build

--- lib/build.zig
+++ lib/build.zig
@@ -4,7 +4,6 @@ const std = @import("std");
 pub const BlstLib = @import("blst.zig");
 pub const CKzgLib = @import("c-kzg.zig");
 pub const Bn254Lib = @import("bn254.zig");
-pub const KeccakLib = @import("keccak.zig");
 
 // Re-export the main functions for convenience
 pub const createBlstLibrary = BlstLib.createBlstLibrary;
