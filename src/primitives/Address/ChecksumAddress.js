ec0f09a9e53e6ccfed00f44bd6d622454abd06c7
William Cory <williamcory@users.noreply.github.com>
2025-12-27 03:18:13 -0800

    ♻️ refactor(address): align Checksummed typedef to HexType

--- src/primitives/Address/ChecksumAddress.js
+++ src/primitives/Address/ChecksumAddress.js
@@ -4,7 +4,7 @@ import { isValid as isValidAddress } from "./isValid.js";
 import { toHex } from "./toHex.js";
 
 /**
- * @typedef {import('../Hex/index.js').Sized<20> & { readonly __variant: 'Address'; readonly __checksummed: true }} Checksummed
+ * @typedef {import('../Hex/HexType.js').HexType & { readonly __variant: 'Address'; readonly __checksummed: true }} Checksummed
  */
 
 /**
