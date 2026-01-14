09bab38dde4fec3f83663f87f50d9d8b9e4b751d
William Cory <willcory10@gmail.com>
2026-01-14 00:23:44 -0800

    fix: organize imports in bn254/index.ts

--- src/crypto/bn254/index.ts
+++ src/crypto/bn254/index.ts
@@ -3,18 +3,18 @@
  * @module crypto/bn254
  */
 export * from "./BN254.js";
-export * from "./G1PointType.js";
-export * from "./G2PointType.js";
-export * from "./Fp2.js";
 export * from "./constants.js";
-export * from "./errors.js";
-export * from "./serializeG1.js";
-export * from "./serializeG2.js";
 export * from "./deserializeG1.js";
 export * from "./deserializeG2.js";
-export * as G1 from "./G1/index.js";
-export * as G2 from "./G2/index.js";
+export * from "./errors.js";
 export * as Fp from "./Fp/index.js";
-export * as Fr from "./Fr/index.js";
 export * as Fp2 from "./Fp2/index.js";
+export * from "./Fp2.js";
+export * as Fr from "./Fr/index.js";
+export * as G1 from "./G1/index.js";
+export * from "./G1PointType.js";
+export * as G2 from "./G2/index.js";
+export * from "./G2PointType.js";
 export * as Pairing from "./Pairing/index.js";
+export * from "./serializeG1.js";
+export * from "./serializeG2.js";
