20de8fbd630bd4c91c08ee34f236cdf9feecbc64
William Cory <willcory10@gmail.com>
2026-02-05 16:50:58 -0800

    🧾 feat: add Effect transaction module

---
+++ packages/voltaire-effect/src/transaction/TransactionStreamError.ts
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview TransactionStream error type for voltaire-effect.
+ *
+ * @module TransactionStreamError
+ * @since 0.2.13
+ */
+
+import * as Data from "effect/Data";
+
+/**
+ * Error thrown when a TransactionStream operation fails.
+ *
+ * @since 0.2.13
+ */
+export class TransactionStreamError extends Data.TaggedError(
+	"TransactionStreamError",
+)<{
+	readonly message: string;
+	readonly cause?: unknown;
+	readonly context?: Record<string, unknown>;
+}> {
+	constructor(
+		message: string,
+		options?: {
+			cause?: unknown;
+			context?: Record<string, unknown>;
+		},
+	) {
+		super({
+			message,
+			cause: options?.cause,
+			context: options?.context,
+		});
+	}
+}
